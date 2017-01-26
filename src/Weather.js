b29832d6ab4263d0ffae8497be4fd56c27d388e3
tpyoung <tylerparkeyoung@gmail.com>
2017-01-25 16:12:25 -0800

    weather data retrived from geoloaction

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,10 @@ export class Weather extends Component {
 		super(props);
 		this. state = {
 			lat: '',
-			lon: ''
+			lon: '',
+			weatherSearch: '',
+			skyConditions: '',
+			temp: ''
 		}
 	}
 
@@ -27,27 +30,35 @@ componentDidMount() {
 				let lon = JSON.stringify(position.coords.longitude);
         this.setState({lon});
         // Creates weather search query
-    		let weatherSearch = 'http://api.openweathermap.org/data/2.5/weather?lat=' + this.state.lat + '&lon=' + this.state.lon + '&appid=8da0bfe263e0d6cdea671f4b23e662bc';
+    		let weatherSearch = 'http://api.openweathermap.org/data/2.5/weather?lat=' + this.state.lat + '&lon=' + this.state.lon + '&units=imperial&appid=8da0bfe263e0d6cdea671f4b23e662bc';
     		this.setState({weatherSearch});
+    		console.log(this.state.weatherSearch);
+		    fetch(this.state.weatherSearch, {
+		    	'headers': {'Accept': 'application/json'}
+		    })
+				.then(res  => res.json())
+				.then(res => this.setState({
+					skyConditions: res.weather[0].main,
+					temp: res.main.temp
+				}))
+				// res.weather[0].main -- sky conditions for image
+				// res.main.temp       -- temperature
+				.catch(err => console.log(err))
       },
       (error) => alert(JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
     );
-  //   fetch(weatherSearch, { 
-  //    	 headers: {'Accept': 'application/json'}
-		// })
-		// .then(res => res.json())
-		// .then(console.log(res))
   }
 
 
   render() {
+
     return (
 <View>
         <Text>
-          <Text style={styles.title}>Initial position: </Text>
-          {this.state.lat}
+          <Text style={styles.title}> {this.state.skyConditions} </Text>
         </Text>
+        <Text>{this.state.temp}°</Text>
       </View>
     );
   }
