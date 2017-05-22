3b8469f6ad5f3743cdd9d5cca6020c812cc6d3da
Tyler Young <tylerparkeyoung@gmail.com>
2017-05-22 13:25:45 -0700

    delete configs.js

--- src/api/configs.js
+++
@@ -1,8 +0,0 @@
-module.exports = 
-	{
-	  apiKey: "AIzaSyDpbrWdjUWHkcpQTymPAapilRMA75z96SQ",
-	  authDomain: "ohaiyo-1f16c.firebaseapp.com",
-	  databaseURL: "https://ohaiyo-1f16c.firebaseio.com",
-	  storageBucket: "gs://ohaiyo-1f16c.appspot.com",
-	  messagingSenderId: "326122708055"
-	}
